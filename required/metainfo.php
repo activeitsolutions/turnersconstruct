90fd98c229d01fbee63af0f35334eec0765a0d46
ForbodingAngel <forbodingangel@gmail.com>
2024-07-10 09:27:27 -0700

    Fix uo the display of metainfo panel

--- required/metainfo.php
+++ required/metainfo.php
@@ -1,18 +1,34 @@
-<?php if(isset($_GET['meta'])) { ?>
+<?php if(isset($_GET['meta'])) { 
+
+function stripQueryFromUrl($url) {
+    $parsedUrl = parse_url($url);
+    $strippedUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+    
+    if (isset($parsedUrl['port'])) {
+        $strippedUrl .= ':' . $parsedUrl['port'];
+    }
+    
+    $strippedUrl .= $parsedUrl['path'];
+    return $strippedUrl;
+}
+
+$strippedUrl = stripQueryFromUrl($currentURL);
+?>
+
 <div id="debug-overlay-container">
 	<div class="debug-content">
 		<strong>Metadata Information for "<?php echo $pagetitle; ?>"</strong></br>
 		<strong>PageTitle:</strong> <?php echo $pagetitle; ?></br>
 		<strong>Layout:</strong> <?php echo $pagelayout; ?></br>
 		<strong>Date:</strong> <?php echo $pagedate; ?></br>
-		<strong>Thumbnail:</strong> <?php echo $WebsiteURL . "/" . $pageimage; ?></br>
+		<strong>Page Image:</strong> <?php echo $WebsiteURL . "/" . $pageimage; ?></br>
 		<strong>Excerpt:</strong> <?php echo $pageexcerpt; ?></br>
 		<strong>Keywords:</strong> <?php echo $pagekeywords; ?></br>
 		<strong>Author:</strong> <?php echo $pageauthor; ?></br>
 		<strong>Type:</strong> <?php echo $pagetype; ?></br>
 		<strong>Website Title:</strong> <?php echo $WebsiteTitle; ?></br>
 		<strong>Language/Country:</strong> <?php echo $WebsiteLanguageCountry; ?></br>
-		<strong>Current URL:</strong> <?php echo $currentURL; ?></br>
+		<strong>Current URL:</strong> <?php echo $strippedUrl; ?></br>
 		<strong>Language/Locale:</strong> <?php echo $WebsiteLanguageLocale; ?></br>
 	</div>
 </div>
